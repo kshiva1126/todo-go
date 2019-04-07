069cc2b9c7f8f7a9f3db62b9eaf21a7a1c25158d
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2019-04-07 21:46:42 +0900

    selectable local or dockercontainer

--- manipulations/manipulation.go
+++ manipulations/manipulation.go
@@ -1,6 +1,11 @@
 package manipulations
 
 import (
+	"fmt"
+	"os"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -11,19 +16,38 @@ type Task struct {
 }
 
 func GormConnect() *gorm.DB {
-	DBMS := "mysql"
-	USER := "root"
-	PASS := "root"
-	PROTOCOL := "tcp(127.0.0.1:3306)"
-	DBNAME := "todo_db"
-
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
-	db, err := gorm.Open(DBMS, CONNECT)
+	connectionString := getConnectionString()
+	db, err := gorm.Open("mysql", connectionString)
 
 	if err != nil {
 		panic(err)
 	}
 	return db
+}
+
+func getConnectionString() string {
+	host := getParamString("MYSQL_DB_HOST", "localhost")
+	port := getParamString("MYSQL_PORT", "3306")
+	user := getParamString("MYSQL_USER", "root")
+	pass := getParamString("MYSQL_PASSWORD", "root")
+	dbname := getParamString("MYSQL_DB", "todo_db")
+	protocol := getParamString("MYSQL_PROTOCOL", "tcp")
+	dbargs := getParamString("MYSQL_DBARGS", " ")
+
+	if strings.Trim(dbargs, " ") != "" {
+		dbargs = "?" + dbargs
+	} else {
+		dbargs = ""
+	}
+	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s", user, pass, protocol, host, port, dbname, dbargs)
+}
+
+func getParamString(param string, defaultValue string) string {
+	env := os.Getenv(param)
+	if env != "" {
+		return env
+	}
+	return defaultValue
 }
 
 func DbGetAll() []Task {
